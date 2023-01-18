d2c0cb71581518dae650bbb25199c60da31e800b
kiksakaiakira <sakai.akira@kik.co.jp>
2023-01-18 11:11:46 +0900

    'updates'

--- pages/product/[pageUrl].tsx
+++ pages/product/[pageUrl].tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Product } from "../../types/product";
 import { ProductDetailRSBody } from "../../types/productDetailRSBody";
 import { getConfig } from "../../Utility/configUtility";
 
 const ProductDetailPage = (rsBody: ProductDetailRSBody) => {
+
+    const router = useRouter();
+
+    if (router.isFallback) {
+      <h1>Data is loading</h1>;
+    }
+
     return (
         <div>
             <h1>{rsBody.product.title}</h1>
@@ -33,31 +41,20 @@ export const getStaticPaths = async () => {
     console.log("products");
     console.log(products);
 
-    const paths = products.map((json: { pageUrl: string; }) => `/product/${json.pageUrl}`);
+    //const paths = products.map((json: { pageUrl: string; }) => `/product/${json.pageUrl}`);
 
-    console.log("index.tsx getStaticProps End");
-    console.log("[pageUrl].tsx getStaticPaths End");
-/*
-    const res : Response = await fetch("http://localhost:3002/product");
-    console.log("------------------------------");
-    console.log("res");
-    console.log(res);
-
-    const json : any = await res.json();
-    console.log("------------------------------");
-    console.log("json");
-    console.log(json);
-
-    const paths = json.map((json: { pageUrl: string; }) => `/product/${json.pageUrl}`);
-
-    console.log("paths");
-    console.log(paths)
+    const paths = products.map((product: { pageUrl: string; }) => ({params: {pageUrl:product.pageUrl}}));
 
     console.log("[pageUrl].tsx getStaticPaths End");
-    console.log("==============================");
-*/
-    // 動的ルート用HTML静的生成
-    return { paths, fallback: false };
+
+    // 動的ルートかつ段階的なHTMLの生成、生成前後の2段階でHTML表示
+    // 対象のページがリクエストされたタイミングでHTMLが生成される
+    // 次回以降のリクエストでは静的生成されたHTMLを返す。
+    // 一回目は即座にビルドされたHTMLが返されるのではなく空っぽのHTMLを返した後にJSで書き換える
+    // ユーザー視点ではloadingの状態を少しはさんで表示される動きになる
+    // 二回目のリクエスト以降はこの動きは発生しない。
+    return { paths, fallback: true };
+    //return { paths: [{ params: { pageUrl: 'SeizaiA' }}, { params: { pageUrl: 'SeizaiB' }}], fallback: true };
 }
 
 // 各ページのレンダリング
